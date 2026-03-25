a279312310dbe361cdea4810dc474a37cb7c3c87
Deepa-Kothandaraman <145525632+Deepa-Kothandaraman@users.noreply.github.com>
2026-03-25 21:54:28 +0530

    Add files via upload

---
+++ Disjoint set.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class Main {
+    static int[] parent;
+    static int[] size;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); 
+        int q = sc.nextInt();
+        parent = new int[n + 1];
+        size = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+        for (int i = 0; i < q; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            union(u, v);
+        }
+        int maxSize = 0;
+        for (int i = 1; i <= n; i++) {
+            if (find(i) == i) { 
+                maxSize = Math.max(maxSize, size[i]);
+            }
+        }
+        System.out.println(maxSize);
+    }
+    static int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+        if (rootA != rootB) {
+            if (size[rootA] < size[rootB]) {
+                parent[rootA] = rootB;
+                size[rootB] += size[rootA];
+            } else {
+                parent[rootB] = rootA;
+                size[rootA] += size[rootB];
+            }
+        }
+    }
+}
